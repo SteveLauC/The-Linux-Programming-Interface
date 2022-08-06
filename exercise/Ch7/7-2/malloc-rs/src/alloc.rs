05a1520998ca5b5b76be2acc0e1570e9729dff78
SteveLauC <stevelauc@outlook.com>
2022-08-06 11:39:04 +0800

    finish 7-2-rs

---
+++ exercise/Ch7/7-2/malloc-rs/src/alloc.rs
@@ -0,0 +1,92 @@
+#![deny(missing_docs)]
+
+//! my memory allocator
+use std::{io::Error, os::raw::c_void, ptr::NonNull};
+
+use libc::sbrk;
+
+#[derive(Debug)]
+/// A memory segment
+pub struct MemorySegment {
+    pub start: NonNull<c_void>,
+    pub size: usize,
+}
+
+impl MemorySegment {
+    /// Construct a new memory segment
+    pub fn new(start: NonNull<c_void>, size: usize) -> Self {
+        MemorySegment { start, size }
+    }
+}
+
+/// A memory allocator
+#[derive(Debug)]
+pub struct Allocator {
+    cur_program_break: usize,
+    free_list: Vec<MemorySegment>,
+}
+
+impl Allocator {
+    /// Construct a new memory allocator
+    pub fn new() -> Self {
+        Allocator {
+            cur_program_break: (unsafe { sbrk(0) }) as usize,
+            free_list: Vec::new(),
+        }
+    }
+
+    /// Merging memory fragments
+    fn compact_free_list(&mut self) {
+        self.free_list
+            .sort_by(|a: &MemorySegment, b: &MemorySegment| a.start.partial_cmp(&b.start).unwrap());
+    }
+
+    /// Allocate memory
+    ///
+    /// The memory returned is **NOT** initialized
+    pub fn malloc(&mut self, size: usize) -> Result<MemorySegment, Error> {
+        // iterate over the free list to find the first memory block whose
+        // size can satify our requirements
+        if let Some(idx) =
+            (0..self.free_list.len()).find(|idx: &usize| self.free_list[*idx].size > size)
+        {
+            let mut chunk: MemorySegment = self.free_list.remove(idx);
+            if chunk.size > size {
+                self.free_list.push(MemorySegment::new(
+                    NonNull::new((chunk.start.as_ptr() as usize + size) as *mut c_void).unwrap(),
+                    chunk.size - size,
+                ));
+            }
+
+            chunk.size = size;
+            return Ok(chunk);
+        }
+
+        // increase the program break to allocate memory
+        if -1_i32 as *mut c_void
+            == unsafe {
+                sbrk(
+                    isize::try_from(size)
+                        .expect("overflow happens when converting usize to isize in sbrk"),
+                )
+            }
+        {
+            return Err(Error::last_os_error());
+        }
+
+        let old_program_break: usize = self.cur_program_break;
+        self.cur_program_break = unsafe { sbrk(0) } as usize;
+        let increment: usize = self.cur_program_break - old_program_break;
+        assert!(increment == size);
+
+        Ok(MemorySegment::new(
+            NonNull::new(old_program_break as *mut c_void).unwrap(),
+            size,
+        ))
+    }
+
+    /// Deallocate memory specified by `mem`.
+    pub fn free(&mut self, mem: MemorySegment) {
+        self.free_list.push(mem);
+    }
+}
---
+++ exercise/Ch7/7-2/malloc-rs/src/main.rs
@@ -0,0 +1,16 @@
+mod alloc;
+
+use alloc::{Allocator, MemorySegment};
+
+fn main() {
+    let mut al: Allocator = Allocator::new();
+    println!("{:?}", al);
+    let new_mem: MemorySegment = al.malloc(4).unwrap();
+    println!("{:?}", new_mem);
+
+    unsafe { (new_mem.start.as_ptr() as *mut i32).write(4) };
+
+    assert_eq!(unsafe { (new_mem.start.as_ptr() as *mut i32).read() }, 4);
+    al.free(new_mem);
+    println!("{:?}", al);
+}
