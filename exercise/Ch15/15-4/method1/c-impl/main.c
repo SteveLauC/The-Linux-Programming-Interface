bd138f22125ea26b421f6502af663442cb9f9ca3
Steve Lau <stevelauc@outlook.com>
2022-10-11 18:48:28 +0800

    fix exercise 15-4 method1

--- exercise/Ch15/15-4/method1/c-impl/main.c
+++ exercise/Ch15/15-4/method1/c-impl/main.c
@@ -12,9 +12,14 @@ int main(void)
 int my_eaccess(const char *path, int mode)
 {
 	uid_t old_r_uid = getuid();
+	gid_t old_r_gid = getgid();
 	uid_t e_uid = geteuid();
+	gid_t e_gid = getegid();
 	assert(0 == setresuid(e_uid, -1, -1));
+	assert(0 == setresgid(e_gid, -1, -1));
 	int ret = access(path, mode);
 	assert(0 == setresuid(old_r_uid, -1, -1));
+	assert(0 == setresgid(old_r_gid, -1, -1));
+
 	return ret;
 }
--- exercise/Ch15/15-4/method1/rs-impl/src/main.rs
+++ exercise/Ch15/15-4/method1/rs-impl/src/main.rs
@@ -1,22 +1,166 @@
 use nix::{
-    libc::uid_t,
-    unistd::{access, geteuid, getuid, setresuid, AccessFlags, Uid},
+    libc::{gid_t, uid_t},
+    unistd::{
+        access, getegid, geteuid, getgid, getuid, setresgid, setresuid, AccessFlags, Gid, Uid,
+    },
     NixPath, Result,
 };
 
-fn main() {
-    my_eaccess("file", AccessFlags::R_OK).unwrap();
-}
+fn main() {}
 
 fn my_eaccess<P>(path: &P, amode: AccessFlags) -> Result<()>
 where
     P: ?Sized + NixPath,
 {
     let old_r_uid = getuid();
+    let old_r_gid = getgid();
     let e_uid = geteuid();
+    let e_gid = getegid();
     setresuid(e_uid, Uid::from(uid_t::MAX), Uid::from(uid_t::MAX)).unwrap();
+    setresgid(e_gid, Gid::from(gid_t::MAX), Gid::from(gid_t::MAX)).unwrap();
     let ret = access(path, amode);
     setresuid(old_r_uid, Uid::from(uid_t::MAX), Uid::from(uid_t::MAX)).unwrap();
+    setresgid(old_r_gid, Gid::from(gid_t::MAX), Gid::from(gid_t::MAX)).unwrap();
 
     ret
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fs::{remove_file, OpenOptions};
+    use std::os::unix::fs::OpenOptionsExt;
+
+    #[test]
+    fn file_that_does_not_exist() {
+        assert!(my_eaccess("file_that_does_not_exist", AccessFlags::F_OK).is_err());
+        assert!(my_eaccess(
+            "file_that_does_not_exist",
+            AccessFlags::F_OK | AccessFlags::R_OK
+        )
+        .is_err());
+        assert!(my_eaccess(
+            "file_that_does_not_exist",
+            AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK
+        )
+        .is_err());
+        assert!(my_eaccess(
+            "file_that_does_not_exist",
+            AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK | AccessFlags::X_OK
+        )
+        .is_err());
+    }
+
+    #[test]
+    fn with_no_permission() {
+        let _ = remove_file("file1");
+        assert!(OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .mode(0o0)
+            .open("file1")
+            .is_ok());
+        assert!(my_eaccess("file1", AccessFlags::F_OK).is_ok());
+        // if privileged, `rw` should be granted
+        if geteuid().is_root() {
+            assert!(my_eaccess("file1", AccessFlags::F_OK | AccessFlags::R_OK).is_ok());
+            assert!(my_eaccess(
+                "file1",
+                AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK
+            )
+            .is_ok());
+        } else {
+            assert!(my_eaccess("file1", AccessFlags::F_OK | AccessFlags::R_OK).is_err());
+            assert!(my_eaccess(
+                "file1",
+                AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK
+            )
+            .is_err());
+        }
+        // `x` is not granted no matter whether this process is privileged
+        assert!(my_eaccess(
+            "file1",
+            AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK | AccessFlags::X_OK
+        )
+        .is_err());
+        assert!(remove_file("file1").is_ok());
+    }
+
+    #[test]
+    fn with_read_permission() {
+        let _ = remove_file("file2");
+        assert!(OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .mode(0o400)
+            .open("file2")
+            .is_ok());
+        assert!(my_eaccess("file2", AccessFlags::F_OK).is_ok());
+        assert!(my_eaccess("file2", AccessFlags::R_OK).is_ok());
+        assert!(my_eaccess("file2", AccessFlags::X_OK).is_err());
+
+        if geteuid().is_root() {
+            assert!(my_eaccess("file2", AccessFlags::W_OK).is_ok());
+        } else {
+            assert!(my_eaccess("file2", AccessFlags::W_OK).is_err());
+        }
+
+        assert!(my_eaccess(
+            "file2",
+            AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK | AccessFlags::X_OK
+        )
+        .is_err());
+        assert!(remove_file("file2").is_ok());
+    }
+
+    #[test]
+    fn with_read_write_permission() {
+        let _ = remove_file("file3");
+        assert!(OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .mode(0o600)
+            .open("file3")
+            .is_ok());
+
+        assert!(my_eaccess("file3", AccessFlags::F_OK).is_ok());
+        assert!(my_eaccess("file3", AccessFlags::R_OK).is_ok());
+        assert!(my_eaccess("file3", AccessFlags::W_OK).is_ok());
+        assert!(my_eaccess("file3", AccessFlags::R_OK | AccessFlags::W_OK).is_ok());
+        assert!(my_eaccess("file3", AccessFlags::R_OK | AccessFlags::X_OK).is_err());
+        assert!(my_eaccess(
+            "file3",
+            AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK | AccessFlags::X_OK
+        )
+        .is_err());
+        assert!(my_eaccess(
+            "file3",
+            AccessFlags::F_OK | AccessFlags::R_OK | AccessFlags::W_OK
+        )
+        .is_ok());
+        assert!(remove_file("file3").is_ok());
+    }
+
+    #[test]
+    fn with_read_write_execute_permission() {
+        let _ = remove_file("file4");
+        // with default umask `0222`, this new file should have permission `0755`
+        assert!(OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .mode(0o777)
+            .open("file4")
+            .is_ok());
+        assert!(my_eaccess("file4", AccessFlags::F_OK).is_ok());
+
+        assert!(my_eaccess("file4", AccessFlags::R_OK).is_ok());
+        assert!(my_eaccess("file4", AccessFlags::R_OK | AccessFlags::W_OK).is_ok());
+        assert!(my_eaccess(
+            "file4",
+            AccessFlags::R_OK | AccessFlags::W_OK | AccessFlags::X_OK
+        )
+        .is_ok());
+
+        assert!(remove_file("file4").is_ok());
+    }
 }
